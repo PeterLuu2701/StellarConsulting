ac1375cefe2a77898ae7d571f1fc7c8c3a5544ed
Yansicong666 <yansicong11@gmail.com>
2025-04-04 13:34:18 -0600

    home page finished

--- FE/StellarConsulting/StellarConsulting/obj/Debug/net8.0/StellarConsulting.AssemblyInfo.cs
+++ FE/StellarConsulting/StellarConsulting/obj/Debug/net8.0/StellarConsulting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StellarConsulting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4de6e8832a95ec995a59b2fe258e6a35770efee4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f24ec02f9b98d00aefb18331899b0e72cca2db3")]
 [assembly: System.Reflection.AssemblyProductAttribute("StellarConsulting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StellarConsulting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
